fd89b56c8467c3bd5a427a97d3e716f26e604cc8
Cherry-Ayala <cherry.devservices@gmail.com>
2023-11-01 15:06:11 -0600

    mgrn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_01_202645) do
+ActiveRecord::Schema.define(version: 2023_11_01_203619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2023_11_01_202645) do
     t.string "author"
     t.string "year"
     t.integer "store_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.string "adress"
+    t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
