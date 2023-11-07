5bc5ed09d15e18ffa25723a1eb95649a1b8cfec1
Cherry-Ayala <cherry.devservices@gmail.com>
2023-11-07 15:31:42 -0600

    migrations,sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
     def create
-        user = User.find_by(email: params[:session][:user].downcase)
+        user = User.find_by(user: params[:session][:user].downcase)
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
             flash[:notice] = "Logged in"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_01_203619) do
+ActiveRecord::Schema.define(version: 2023_11_05_222722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2023_11_01_203619) do
     t.string "name"
     t.string "adress"
     t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
